7d4ed662bbac38ef9a84f67ec140b298f30d8bc2
Andrea Pena <apena9@uci.edu>
2025-10-31 08:37:23 -0700

    using the stemming process taught in class to get the top 50 words and most common.

--- tokenizer.py
+++ tokenizer.py
@@ -8,19 +8,22 @@ from nltk.corpus import stopwords
 
 nltk.download('stopwords', quiet=True)
 STOPWRDS = set(stopwords.words('english'))
-stemmer = KrovetzStemmer()
+stemmer = KrovetzStemmer() # making use of the stemmer taught in class
 
 def tokenize(text):
     try:
+       
+    #Parse the raw HTML/XML content using lxml parser
        soup = BeautifulSoup(text.raw_response.content, "lxml")
-       for tag in soup(["script", "style"]):  
+
+       for tag in soup(["script", "style"]):  # Remove script and style elements since they don't contain meaningful text
             tag.extract()
        text = soup.get_text(separator=' ')
        parsed = []
        word_builder = ''
        for c in text:
             if c == "'" or c == "-" or c.isalnum():
-                word_builder += c
+                word_builder += c # Add valid characters (letters, numbers, apostrophes, or hyphens) to the current word
             else:
                 if word_builder.endswith("'s"):
                     word_builder = word_builder[:-2]
@@ -33,12 +36,15 @@ def tokenize(text):
             if word_builder and word_builder.isascii():
                 parsed.append(word_builder.lower())
 
-
             parsed_info = [t for t in parsed if t not in STOPWRDS and len(t) > 1]
+            # Apply Krovetz stemming
             stemmed_tokens = [stemmer.stem(t) for t in parsed_info]
             counter = Counter(stemmed_tokens)
             top_50 = counter.most_common(50)
-    except FileNotFoundError:
-        print(f" File '{file_path}' not found.")
-        sys.exit(1)
-    return parsed
+            for i, (word, count) in enumerate(top_50, start=1):
+                print(f"{i:2}. {word:<15} {count}")
+
+
+    except Exception as e:
+        print(f"Error found {e}")
+    return parsed #return a list of the elements
