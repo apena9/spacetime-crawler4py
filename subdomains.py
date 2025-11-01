320244f678499133e7d1b0a58774932871aaf595
Yoseph Kim <yosephmk@uci.edu>
2025-10-31 22:51:08 -0700

    new module to identify and count occurences of subdomains

---
+++ subdomains.py
@@ -0,0 +1,32 @@
+from utils import get_logger
+import tldextract # pip install tldextract
+
+
+subdomains = dict() # string, int key value pairs (url, count)
+subdomains_logger = get_logger('Subdomains')
+
+def update_subdomains(scraped_urls):
+    '''
+    uses scraped_urls and marks the urls that it has seen.
+    '''
+    for scraped_url in scraped_urls:
+        ext = tldextract.extract(scraped_url)
+        if ext.subdomain:
+            full_subdomain = f'{ext.subdomain}.{ext.domain}'
+            try:
+                subdomains[full_subdomain] += 1
+                subdomains_logger.info(f"UPDATING COUNT: '{full_subdomain}': {subdomains[full_subdomain]} occurences")
+            except KeyError:
+                subdomains[full_subdomain] = 1
+                subdomains_logger.info(f"NEW SUBDOMAIN: '{full_subdomain}'")
+
+def get_total_subdomains():
+    '''
+    writes out subdomain and its count. Called after crawl is completed.
+    sorted from most occuring subdomains to least occuring.
+    '''
+    sorted_subdomains = sorted(subdomains.items(), key=lambda item: item[1])
+    for item in sorted_subdomains:
+        subdomains_logger.info(
+                f"Subdomain: {item[0]} --> {item[1]}")
+        
