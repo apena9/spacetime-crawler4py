1175d515ef89d524db5fe86c06807826862bb9e8
Yoseph Kim <yosephmk@uci.edu>
2025-11-04 01:20:06 -0800

    reporting added after crawl

--- launch.py
+++ launch.py
@@ -4,6 +4,7 @@ from argparse import ArgumentParser
 from utils.server_registration import get_cache_server
 from utils.config import Config
 from crawler import Crawler
+from utils.report import report_all
 
 
 def main(config_file, restart):
@@ -22,3 +23,4 @@ if __name__ == "__main__":
     parser.add_argument("--config_file", type=str, default="config.ini")
     args = parser.parse_args()
     main(args.config_file, args.restart)
+    report_all('Report')
